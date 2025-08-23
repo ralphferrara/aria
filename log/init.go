fcb085a0af7a83b4db563110bb503f63940b893a
socialcomedia <socialcomedia@gmail.com>
2025-08-22 17:44:50 -0700

    Merged Log into main package

--- log/init.go
+++ log/init.go
@@ -1,22 +1,93 @@
 //||------------------------------------------------------------------------------------------------||
-//|| Log Package: Initialization
+//|| Log Package: Initialization (All-in-One)
 //|| init.go
 //||------------------------------------------------------------------------------------------------||
 
 package log
 
-import "aria/config"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"aria/config"
+)
 
 //||------------------------------------------------------------------------------------------------||
-//|| Log: Global Vars
+//|| Log: Level Type and Constants
+//||------------------------------------------------------------------------------------------------||
+
+type Level string
+
+const (
+	INFO  Level = "INFO"
+	WARN  Level = "WARN"
+	ERROR Level = "ERROR"
+	DEBUG Level = "DEBUG"
+)
+
+//||------------------------------------------------------------------------------------------------||
+//|| Log: Global Config Reference
 //||------------------------------------------------------------------------------------------------||
 
 var AppConfig *config.Config
 
-//||------------------------------------------------------------------------------------------------||
-//|| Log: Global Vars
-//||------------------------------------------------------------------------------------------------||
-
 func Init(cfg *config.Config) {
 	AppConfig = cfg
+}
+
+//||------------------------------------------------------------------------------------------------||
+//|| Log: Print (General Logger)
+//||------------------------------------------------------------------------------------------------||
+
+func Print(level Level, module string, msg string, args ...interface{}) {
+	if !shouldLog(level) {
+		return
+	}
+	now := time.Now().Format("2006-01-02 15:04:05")
+	fullMsg := fmt.Sprintf(msg, args...)
+	line := fmt.Sprintf("[%s] [%s] [%s] %s\n", now, level, module, fullMsg)
+	if level == ERROR {
+		fmt.Fprint(os.Stderr, line)
+	} else {
+		fmt.Print(line)
+	}
+}
+
+//||------------------------------------------------------------------------------------------------||
+//|| Log: Level-Specific Shortcuts
+//||------------------------------------------------------------------------------------------------||
+
+func Info(module, msg string, args ...interface{})  { Print(INFO, module, msg, args...) }
+func Warn(module, msg string, args ...interface{})  { Print(WARN, module, msg, args...) }
+func Error(module, msg string, args ...interface{}) { Print(ERROR, module, msg, args...) }
+func Debug(module, msg string, args ...interface{}) { Print(DEBUG, module, msg, args...) }
+
+//||------------------------------------------------------------------------------------------------||
+//|| Log: Level Control Helper
+//||------------------------------------------------------------------------------------------------||
+
+func shouldLog(level Level) bool {
+	// Example: log all for now (expand for config-based filtering)
+	return true
+}
+
+//||------------------------------------------------------------------------------------------------||
+//|| Log: Facade Struct for Log.Info-style calls
+//||------------------------------------------------------------------------------------------------||
+
+var Log = struct {
+	Info      func(module, msg string, args ...interface{})
+	Warn      func(module, msg string, args ...interface{})
+	Error     func(module, msg string, args ...interface{})
+	Debug     func(module, msg string, args ...interface{})
+	Init      func(cfg *config.Config)
+	AppConfig **config.Config
+}{
+	Info:      Info,
+	Warn:      Warn,
+	Error:     Error,
+	Debug:     Debug,
+	Init:      Init,
+	AppConfig: &AppConfig,
 }
